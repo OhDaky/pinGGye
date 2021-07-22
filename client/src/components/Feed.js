fae2b3e8c6869615bb8cfa2828367cbf8f7338c0
jeongbeen2 <aceman9508@gmail.com>
2021-07-22 15:41:40 +0900

    [Feat] 피드상세페이지 구조 제작

--- client/src/components/Feed.js
+++ client/src/components/Feed.js
@@ -1,5 +1,49 @@
 import React from "react";
+import "./Styles/Feed.css";
+import FeedComment from "./FeedComment";
 
 export default function Feed() {
-  return <div>feed</div>;
+  let comment = 10;
+  return (
+    <>
+      <div className="main">
+        <div className="feed__content">
+          <div className="feed__left-content">
+            <div className="feed__main-img">
+              <div className="feed__main-img__topbar" />
+              <h1>사진</h1>
+              <div className="feed__main-img__underbar">
+                <div className="feed__main-img__underbar__left-content">
+                  #해시태그
+                </div>
+                <div className="feed__main-img__underbar__right-content">
+                  계란!
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="feed__right-content">
+            <div className="feed__main-contents">
+              <div className="feed__writter">글 올린 사람</div>
+              <div className="feed__main-content">사진에 대한 내용</div>
+            </div>
+            <div className="feed__comments">
+              <div className="input-comment">
+                <input type="text" placeholder="댓글 입력창" />
+                <button className="comment__btn">댓글 달기</button>
+              </div>
+              <div className="feed__comments-box">
+                <FeedComment />
+                <FeedComment />
+                <FeedComment />
+                <FeedComment />
+                <FeedComment />
+                <FeedComment />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
