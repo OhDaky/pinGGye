e7c093922a0de31838754334697be36a770642f8
jeongbeen2 <aceman9508@gmail.com>
2021-07-22 14:06:38 +0900

    [Feat] 마이페이지 구조 제작

--- client/src/components/Mypage.js
+++ client/src/components/Mypage.js
@@ -1,5 +1,31 @@
 import React from "react";
+import "./Styles/Mypage.css";
 
 export default function Mypage() {
-  return <div>mypage</div>;
+  return (
+    <>
+      <div className="main">
+        <div className="mypage">
+          <div className="mypage__message">마이 페이지</div>
+          <input
+            className="input__email"
+            type="email"
+            placeholder="Email"
+            readonly
+          />
+          <input
+            className="input__nickname"
+            type="text"
+            placeholder="Nickname"
+          />
+          <input
+            className="input__password"
+            type="password"
+            placeholder="Password"
+          />
+          <button className="mypage__btn">회원 정보 수정</button>
+        </div>
+      </div>
+    </>
+  );
 }
