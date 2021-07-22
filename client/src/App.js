9e7ee2ef2bd350812738ce08f0f1eb34931f4203
jeongbeen2 <aceman9508@gmail.com>
2021-07-22 10:53:41 +0900

    [Feat] AWS 초기 설정

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,13 @@
+import axios from "axios";
+
 function App() {
+  axios
+    .get(`${process.env.REACT_APP_API_URL}/`)
+    .then((res) => {
+      console.log(res);
+    })
+    .catch((err) => console.error(err));
+
   return (
     <>
       <div>Hello pinGGye!</div>
