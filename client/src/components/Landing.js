b9b3efe103cf99be1d140e5cfc697e450f8b9c05
OhDaky <ttakko88@gmail.com>
2021-07-22 16:53:19 +0900

    [Feat] 랜딩페이지 구조 제작

--- client/src/components/Landing.js
+++ client/src/components/Landing.js
@@ -1,5 +1,10 @@
 import React from "react";
+import "./Styles/Landing.css"
 
 export default function Landing() {
-  return <div>landing</div>;
+  return (
+    <center>
+      <div className="landing-img">landing img</div>
+    </center>
+  )
 }
