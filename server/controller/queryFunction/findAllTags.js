e45a20dd4d113db627234a57af6aa8eff8a230ca
jeyoon2 <wpdbs4419@gmail.com>
2021-07-23 20:09:49 +0900

    to pull upstream

---
+++ server/controller/queryFunction/findAllTags.js
@@ -0,0 +1,31 @@
+const { Feed: FeedModel, Tag: TagModel } = require("../../models");
+
+module.exports = async (req, res) => {
+  //* COUNT 쿼리 방법
+  // const tags = await TagModel.findAll({
+  //   attributes: ["id", "name", sequelize.fn("COUNT", sequelize.col("Feeds.id"))],
+  //   include: [
+  //     { model: FeedModel, attributes: [], through: { attributes: [] } }
+  //   ],
+  //   group: "Feeds.id",
+  //   having: sequelize.where(sequelize.fn("COUNT", sequelize.col("Feeds.id")), '>=', 1)
+  // });
+
+  //* 조인 후 처리
+  const tags = await TagModel.findAll({
+    attributes: ["id", "name"],
+    include: [
+      { model: FeedModel, attributes: ["id"], through: { attributes: [] } },
+    ],
+  });
+  const formattedTags = tags
+    .map((tag) => {
+      const feedCount = tag.dataValues.Feeds.length;
+      tag.dataValues.feedCount = feedCount;
+      delete tag.dataValues.Feeds;
+      return tag.dataValues;
+    })
+    .filter((tag) => tag.feedCount > 0);
+
+  return formattedTags;
+};
---
+++ server/controller/queryFunction/index.js
@@ -0,0 +1,6 @@
+module.exports = {
+  findAllFeeds: require("./findAllFeeds"),
+  findAllTags: require("./findAllTags"),
+  findFeed: require("./findFeed"),
+  findFeedComments: require("./findFeedComments"),
+};
