0b84a37d2dd87dc18a760f0a67f4f048224dea48
OhDaky <ttakko88@gmail.com>
2021-07-23 14:58:19 +0900

    [Fixed] 로그인 css 수정

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -31,31 +31,31 @@ export default function Login({ handleResponseSuccess }) {
   return (
     <>
     <div className="login__container">
-    <div className="login__img-box">로그인 페이지 이미지</div>
-    <div className="login__login-box">
-      <div className="login__container-title">Login</div>
-      <form onSubmit={(e) => e.preventDefault()}>
-        <div className="login__input-email">
-          <div>이메일</div>
-          <input type="email" onChange={handleInputValue("email")}></input>
+      <div className="login__img-box">로그인 페이지 이미지 예시</div>
+      <div className="login__login-box">
+        <div className="login__container-title">Login</div>
+        <form onSubmit={(e) => e.preventDefault()}>
+          <div className="login__input-email">
+            <div>이메일</div>
+            <input type="email" onChange={handleInputValue("email")}></input>
+          </div>
+          <div className="login__input-password">
+            <div>비밀번호</div>
+              <input type="password" onChange={handleInputValue("password")}/>
+          </div>
+          <div>
+            <button className="login__btn-login" type="submit" onClick={handleLogin}>
+              로그인
+            </button>
+          </div>
+          <div>
+            <button className="login__btn-signup"><Link to="/signup">회원가입</Link></button>
+          </div>
+          </form>
+          <div className="login__alert-box">{errorMessage}</div>
         </div>
-        <div className="login__input-password">
-          <div>비밀번호</div>
-          <input type="password" onChange={handleInputValue("password")}/>
-        </div>
-        <div>
-          <button className="login__btn-login" type="submit" onClick={handleLogin}>
-            로그인
-          </button>
-        </div>
-        <div>
-          <button className="login__btn-signup"><Link to="/signup">회원가입</Link></button>
-        </div>
-        <div className="login__alert-box">{errorMessage}</div>
-      </form>
       </div>
-    </div>
-      <Footer></Footer>
+      <Footer />
     </>
   );
 }
